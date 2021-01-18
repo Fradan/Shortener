6c3fa08a9c3c18a7aabc602acfdd8e5f46345662
roumen14 <roumen14@yandex.ru>
2021-01-19 00:32:51 +0300

    Небольшое изменение сущности. Добавление атрибута для корректной работы Id, добавлено поле BackHalf для хранения сгенерированного значения.

--- Core/Shortener.cs
+++ Core/Shortener.cs
@@ -1,13 +1,19 @@
 ﻿
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Core
 {
     public class Shortener
     {
-        public int Id;
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id;
 
         public string ShortLink;
 
         public string SourceLink;
+
+        public string BackHalf;
 
         public int Counter;
     }
